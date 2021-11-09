9eed09b208d809bb199fb6af61671f0f0043ac0b
Abhi <81672229+Hatred0001@users.noreply.github.com>
2021-11-09 21:31:51 +0530

    page commit 2

--- Styling Assets/javascript files/background-change-2.js
+++
@@ -1,7 +0,0 @@
-
-$(window).scroll(function() {
-    var scroll = $(window).scrollTop();
-    $(".").css({
-      transform: 'translate3d(0, +'+(scroll/100)+'%, 0) scale('+(110- scroll/120)/80+')'
-    });
-  });
